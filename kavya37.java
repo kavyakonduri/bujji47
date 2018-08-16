6510bf671cd5e420cee15db97809f5128d4febd3
kavyakonduri <41731461+kavyakonduri@users.noreply.github.com>
2018-08-16 12:23:00 +0530

    Create kavya37.java

---
+++ kavya37.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s1="wel";
+        String s2="come";
+        String s3=s1+s2;
+        System.out.println("after concatination"+s3);
+	}
+}
